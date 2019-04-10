a5bd9acc448f4d72cfe35892ec275b84f099acf6
rpf1980 <united_ssh@outlook.com>
2019-04-10 20:38:10 +0200

    Cambios en el ejercicio. Ahora el fichero copia tiene el nombre de cada uno de los ficheros a copiar separados de un guión bajo

--- UnificaContenidoFicherosCopia/src/com/company/Main.java
+++ UnificaContenidoFicherosCopia/src/com/company/Main.java
@@ -13,9 +13,21 @@ public class Main
         List <String> lista = new ArrayList<>();
         int i;
 
-        nombreFicheroDestino="C.txt";
+        String[] arrayRutaFichero1 = rutaFichero1.split("\\\\");
+        String[] arrayRutaFichero2 = rutaFichero2.split("\\\\");
+        String nombreFichero1 = arrayRutaFichero1[arrayRutaFichero1.length-1]; //Guardamos nombre completo del fichero1 (fichero1.txt)
+        String nombreFichero2 = arrayRutaFichero2[arrayRutaFichero2.length-1]; //Guardamos nombre completo del fichero2 (fichero2.txt)
+
+        int pos = nombreFichero1.indexOf('.');
+
+        String soloNombre1 = nombreFichero1.substring(0, pos); //Gurdamos solo el nombre sin extensión (fichero1)
+        String soloNombre2 = nombreFichero2.substring(0, pos); //Gurdamos solo el nombre sin extensión (fichero2)
+
+
+        nombreFicheroDestino=soloNombre1 + "_" + soloNombre2 + ".txt"; //Nombre completo de nuestro fichero copia
         try
         {
+            //Leemos primer fichero y copiamos contenido en una lista
             FileReader fr = new FileReader(rutaFichero1);
             BufferedReader br = new BufferedReader(fr);
 
@@ -30,6 +42,7 @@ public class Main
             br.close();
             fr.close();
 
+            //Leemos segundo fichero y guardamos contenido en la lista
             FileReader fr2 = new FileReader(rutaFichero2);
             BufferedReader br2 = new BufferedReader(fr2);
 
@@ -47,11 +60,17 @@ public class Main
             FileWriter fw = new FileWriter(rutaDestino+nombreFicheroDestino);
             BufferedWriter bw = new BufferedWriter(fw);
 
-            for (i=0;i<lista.size();i++)
+            //Recorremos la lista que contiene los datos de los dos ficheros y lo copiamos en el fichero de destino
+            for(String s:lista)
+            {
+                bw.write(s);
+                bw.newLine();
+            }
+            /*for (i=0;i<lista.size();i++)
             {
                 bw.write(lista.get(i));
                 bw.newLine();
-            }
+            }*/
 
             bw.close();
             fw.close();
@@ -63,7 +82,6 @@ public class Main
     }
     public static void main(String[] args)
     {
-        copiarDosFicheroEnUno("D:\\DAW_1\\fichero1.txt","D:\\fichero2.txt","D:\\DESTINO\\");
+        copiarDosFicheroEnUno("D:\\varios\\fichero1.txt","D:\\videos\\fichero2.txt","D:\\destinoCOPIA\\");
     }
 }
-
